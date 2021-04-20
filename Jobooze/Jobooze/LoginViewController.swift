bad739bcbf3b4aaa5a890c71d8304b24c0909ed2
Nicholas Palermo <nicholas.palermo2@cix.csi.cuny.edu>
2021-04-20 16:37:17 -0400

    First Commit – Includes: - Starter files - Basic Login View Controller - no function; - Basic Signup View Controller - no function;

---
+++ Jobooze/Jobooze/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.swift
+//  Jobooze
+//
+//  Created by Nicholas Palermo on 4/20/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        performSegue(withIdentifier: "ToSignUp", sender: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Jobooze/Jobooze/SignUpViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SignUpViewController.swift
+//  Jobooze
+//
+//  Created by Nicholas Palermo on 4/20/21.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func OnSignIn(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
