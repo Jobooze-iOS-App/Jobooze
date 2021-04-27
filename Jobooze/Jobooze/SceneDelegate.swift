bc9e8d89495d3d62e475a47a354e3b8d12ac082c
abdel-elsayed <abdel.elsayed18@gmail.com>
2021-04-27 16:20:09 -0400

    DEV: added profile picture for user and a placeholder.

--- Jobooze/Jobooze/SceneDelegate.swift
+++ Jobooze/Jobooze/SceneDelegate.swift
@@ -16,23 +16,23 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        guard let _ = (scene as? UIWindowScene) else { return }
         
         NotificationCenter.default.addObserver(forName: Notification.Name("login"), object: nil, queue: OperationQueue.main) { (Notification) in
             print("Login Notification Received!")
             self.login()
         }
         
-//        uncomment this once we have a logout button so that the user can stay logged in
-//        if (PFUser.current() != nil) {
-//            login()
-//        }
+        if (PFUser.current() != nil) {
+            login()
+        }
         
         guard let _ = (scene as? UIWindowScene) else { return }
     }
     
     func login() {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        window?.rootViewController = storyboard.instantiateViewController(withIdentifier: "TabBarController")
+        window?.rootViewController = storyboard.instantiateViewController(withIdentifier: "")
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
